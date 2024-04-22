6bd1b463c5961cc859706b94ea9859dba281f160
VASUDHA NARAYANAN <s-vanarayanan@lwsd.org>
2024-04-22 10:09:49 -0700

    putting code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import './App.css';
 
+import { Login } from './pages';
+
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      } catch(e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
+      <GlobalStyle />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!token ? (
+          <Login />
+        ) : (
+          <>
+            <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar"/>
+                )}
+              </div>
+            )}
+          </>
+        )}
       </header>
     </div>
   );
